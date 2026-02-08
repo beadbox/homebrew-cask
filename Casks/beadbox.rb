5afd86c327290a7072314da6941b6d84fdeab98f
Nelson Melo <nmelo@users.noreply.github.com>
2026-02-08 14:26:08 -0500

    beadbox: update to 0.3.0

--- Casks/beadbox.rb
+++ Casks/beadbox.rb
@@ -1,8 +1,8 @@
 cask "beadbox" do
   version "0.3.0"
-  sha256 "TODO"
+  sha256 "5161d86f0a1645884e745824683418c4602f8dc9ee71a9e816591f8e86f7be04"
 
-  url "https://github.com/nmelo/beadbox/releases/download/v#{version}/Beads-#{version}-arm64.dmg"
+  url "https://github.com/nmelo/beadbox/releases/download/v0.3.0/Beads-0.3.0-arm64.dmg"
   name "Beadbox"
   desc "Desktop app for the beads issue tracker"
   homepage "https://github.com/beadbox/beadbox"
