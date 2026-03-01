20960fa995d3f6be5a5369452fe1d8befad9aaa3
Beadbox <hello@beadbox.app>
2026-02-28 23:14:04 -0500

    Homebrew cask for Beadbox

---
+++ Casks/beadbox.rb
@@ -0,0 +1,30 @@
+cask "beadbox" do
+  version "0.12.0"
+
+  on_arm do
+    sha256 "9cf0ea441087c5e3b2ed15ed9023f7c23886984a2a55f64c40ab917e5ff541aa"
+    url "https://github.com/beadbox/beadbox/releases/download/v#{version}/Beadbox-#{version}-macOS-arm64.dmg"
+  end
+
+  on_intel do
+    sha256 "a59c8e8171fc0239e7c989a9dc3ed74f2e681019f0e642d62a2cd0a655617c71"
+    url "https://github.com/beadbox/beadbox/releases/download/v#{version}/Beadbox-#{version}-macOS-x64.dmg"
+  end
+
+  name "Beadbox"
+  desc "Desktop app for the beads issue tracker"
+  homepage "https://github.com/beadbox/beadbox"
+
+  depends_on macos: ">= :ventura"
+
+  app "Beadbox.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.nmelo.beadbox",
+    "~/Library/Caches/com.nmelo.beadbox",
+    "~/Library/Preferences/com.nmelo.beadbox.plist",
+    "~/Library/Application Support/com.nmelo.beads-ui",
+    "~/Library/Caches/com.nmelo.beads-ui",
+    "~/Library/Preferences/com.nmelo.beads-ui.plist",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,22 @@
+# Beadbox Homebrew Cask
+
+Homebrew cask for [Beadbox](https://github.com/beadbox/beadbox), the macOS app for the beads issue tracker.
+
+## Install
+
+```bash
+brew tap beadbox/cask
+brew install --cask beadbox
+```
+
+## Update
+
+```bash
+brew upgrade --cask beadbox
+```
+
+## Uninstall
+
+```bash
+brew uninstall --cask beadbox
+```
