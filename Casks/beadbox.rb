0599885701a296c2e13da91f520c5619c2557825
Nelson Melo <238738+nmelo@users.noreply.github.com>
2026-04-02 22:56:39 -0400

    chore: bump beadbox to v0.20.1

--- Casks/beadbox.rb
+++ Casks/beadbox.rb
@@ -1,9 +1,9 @@
 cask "beadbox" do
-  version "0.20.0"
+  version "0.20.1"
 
   arch arm: "arm64"
 
-  sha256 "f852bbf90db404450727b04a52169fdf2ddd7661c57e20c2a88e97c2e94f0d1d"
+  sha256 "7d02c97f9ff9b4f20eead4542083f35f8a4fe69ca9c30193316a32660d0106f0"
   url "https://github.com/beadbox/beadbox/releases/download/v#{version}/Beadbox-#{version}-macOS-arm64.dmg"
 
   name "Beadbox"
